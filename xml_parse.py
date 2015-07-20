7d741ba429de97cd52c9bb377dcaa9e540e4ffdd
chainsawriot <chainsawtiney@gmail.com>
2015-07-21 02:05:00 +0800

    Parser can basically parse all fields in Legco XML

--- xml_parse.py
+++ xml_parse.py
@@ -17,6 +17,18 @@ def parse_meeting(meeting_tree):
 def fg_text(tag_name, tree):
     return tree.find(tag_name).get_text()
 
+def extract_tags(tags, tree, res):
+    for tag in tags:
+        res[re.sub(re.escape("-"), "_", tag)] = fg_text(tag, tree)
+    return res
+
+def extract_member(member):
+    inv = {}
+    for key in ['constituency', 'name-ch', 'name-en']:
+        inv[re.sub(re.escape("-"), "_", key)] = member[key]
+    inv['vote'] = member.find('vote').get_text()
+    return inv
+
 # TODO:
 # vote-date and vote-time should be combined to vote-datetime
 # mover-ch and mover-en and mover-type should be pushed into inviduals
@@ -24,16 +36,21 @@ def fg_text(tag_name, tree):
 def parse_vote(vote_tree):
     res = {}
     res['number'] = vote_tree['number'] # I don't think this value is important
-    for tag in ["vote-date", "vote-time", "motion-ch", "motion-en", 'mover-ch', 'mover-en', 'mover-type', 'vote-separate-mechanism']:
-        res[re.sub(re.escape("-"), "_", tag)] = fg_text(tag, vote_tree)
-        # similarly if the movers are mover-type is Public Officer then vote-sep-mec is No.
-        # Unless future change in counting (which is extremely unlikely). 
-        # Still I will keep that
+    res = extract_tags(["vote-date", "vote-time", "motion-ch", "motion-en", 'mover-ch', 'mover-en', 'mover-type', 'vote-separate-mechanism'], vote_tree, res)
+    # similarly if the movers are mover-type is Public Officer then vote-sep-mec is No.
+    # Unless future change in counting (which is extremely unlikely). 
+    # Still I will keep that
     vote_summary_tree = vote_tree.find("vote-summary")
+    res['summaries'] = []
     for summary in vote_summary_tree.contents:
         vote_summary = {}
         vote_summary['type'] = summary.name
-        for tag in ['present-count', 'vote-count', 'yes-count', 'no-count', 'abstain-count', 'result']:
-            vote_summary[re.sub(re.escape("-"), "_", tag)] = fg_text(tag, summary)
-        print vote_summary
+        vote_summary = extract_tags(['present-count', 'vote-count', 'yes-count', 'no-count', 'abstain-count', 'result'], summary, vote_summary)
+        res['summaries'].append(vote_summary)
+    res['individual_votes'] = []
+    for member in vote_tree.find("individual-votes").find_all("member"):
+        inv = extract_member(member)
+        res['individual_votes'].append(inv)
     return res
+
+print parse_meeting(vote_bs.find('meeting'))
